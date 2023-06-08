b1ec2dc318e32b53579dd14347e11982f6b69073
GreyWolnick <52846222+GreyWolnick@users.noreply.github.com>
2023-06-08 16:49:25 -0700

    added mac compilability

--- Makefile
+++ Makefile
@@ -2,4 +2,4 @@ windows:
 	g++ -Iinclude -Iwindows_include -L. sav3dplay.cpp -lglfw3 -lglew32 -lopengl32 -lgdi32 -luser32 -lkernel32 -lsav1 -o sav3dplay.exe
 
 mac:
-	echo "coming soon..."
+	g++ -Iinclude -Iwindows_include -L. sav3dplay.cpp -lglfw -lGLEW -lsav1 -framework OpenGL -o sav3dplay
--- sav3dplay.cpp
+++ sav3dplay.cpp
@@ -1,8 +1,6 @@
 #include "sav1.h"
 
-#ifdef _WIN32
 #include <GL/glew.h>
-#endif
 
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
@@ -229,6 +227,7 @@ main(int argc, char *argv[])
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
     // define window size
     int width = 1000;
@@ -244,14 +243,12 @@ main(int argc, char *argv[])
 
     glfwMakeContextCurrent(window);
 
-#ifdef _WIN32
     // initialize GLEW
     if (glewInit() != GLEW_OK) {
         std::cerr << "Failed to initialize GLEW" << std::endl;
         glfwTerminate();
         return 1;
     }
-#endif
 
     // make sure the escape key can be captured
     glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
